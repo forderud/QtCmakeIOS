5998622b7f6e0c9a503e961e160dfb359c9ec9bd
Fredrik Orderud <forderud@gmail.com>
2021-05-25 13:26:29 +0200

    auto-open project in Xcode

--- Build.sh
+++ Build.sh
@@ -9,3 +9,6 @@ git pull
 
 echo Creating CMake project...
 cmake -G Xcode -DCMAKE_TOOLCHAIN_FILE=$IOS_TOOLCHAIN -DPLATFORM=OS64 -DARCHS=arm64 -DENABLE_BITCODE=0 -DDEPLOYMENT_TARGET=12.0 -DENABLE_VISIBILITY=1 -DENABLE_ARC=0 -DCMAKE_SYSTEM_VERSION=14.0 .
+
+echo Opening project in Xcode...
+open analogclock.xcodeproj
